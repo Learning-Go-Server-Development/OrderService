7266b374fd73b3f9a8f6d1fd7edaa00d3086168a
KenWilliamson <kenulborapublic@gmail.com>
2026-02-06 11:00:49 -0500

    update

---
+++ handlers/itemHandler.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Learning-Go-Server-Development/OrderService/manager"
+)
+
+func (h *ServiceHandler) AddItem(w http.ResponseWriter, r *http.Request) {
+	h.setContentType(w)
+	bcOk := h.checkContent(r)
+	if !bcOk {
+		http.Error(w, "json required", http.StatusUnsupportedMediaType)
+	} else {
+		var i manager.Item
+		pi, err := h.processBody(r, &i)
+		log.Println("bs: ", pi)
+		log.Println("err: ", err)
+		if !pi || err != nil {
+			http.Error(w, "Trouble parsing body", http.StatusBadRequest)
+		} else {
+			ir := h.Manager.AddItem(&i)
+			log.Println("oo: ", ir)
+			if ir.Success && ir.ID != 0 {
+				w.WriteHeader(http.StatusOK)
+				resJSON, _ := json.Marshal(ir)
+				fmt.Fprint(w, string(resJSON))
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+	}
+}
---
+++ handlers/itemHandler_test.go
@@ -0,0 +1,67 @@
+package handlers_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Learning-Go-Server-Development/OrderService/handlers"
+	"github.com/Learning-Go-Server-Development/OrderService/manager"
+)
+
+func TestServiceHandler_AddItem(t *testing.T) {
+	var mm manager.MockServiceManager
+	m := mm.New()
+	var hh handlers.ServiceHandler
+	hh.Manager = m
+
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		w     http.ResponseWriter
+		r     *http.Request
+		res   manager.ResponseID
+		code  int
+		ctype string
+		json  io.ReadCloser
+		suc   bool
+		want  int64
+		want2 bool
+	}{
+		// TODO: Add test cases.
+		{
+			code:  200,
+			ctype: "application/json",
+			json:  io.NopCloser(bytes.NewBufferString(`{"orderId":1, "productId": 5}`)),
+			suc:   true,
+			want:  1,
+			want2: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mm.AddItemRes = &manager.ResponseID{
+				ID:      tt.want,
+				Success: tt.want2,
+			}
+			// aJSON := io.NopCloser(bytes.NewBufferString(`{"orderNumber":"OD-11123", "customerID": 5}`))
+
+			r, _ := http.NewRequest("POST", "/ffllist", tt.json)
+			r.Header.Set("Content-Type", tt.ctype)
+			w := httptest.NewRecorder()
+			// TODO: construct the receiver type.
+			//var h handlers.ServiceHandler
+			hh.AddItem(w, r)
+
+			var res manager.ResponseID
+			body, _ := io.ReadAll(w.Result().Body)
+			json.Unmarshal(body, &res)
+			if w.Code != tt.code || res.Success != tt.suc {
+				t.Fail()
+			}
+		})
+	}
+}
