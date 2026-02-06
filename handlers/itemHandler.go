aca9deeaa763a61d01aac5d5547275e7cdd5a07c
KenWilliamson <kenulborapublic@gmail.com>
2026-02-06 11:14:38 -0500

    update

--- handlers/itemHandler.go
+++ handlers/itemHandler.go
@@ -11,8 +11,8 @@ import (
 
 func (h *ServiceHandler) AddItem(w http.ResponseWriter, r *http.Request) {
 	h.setContentType(w)
-	bcOk := h.checkContent(r)
-	if !bcOk {
+	Ok := h.checkContent(r)
+	if !Ok {
 		http.Error(w, "json required", http.StatusUnsupportedMediaType)
 	} else {
 		var i manager.Item
@@ -25,6 +25,32 @@ func (h *ServiceHandler) AddItem(w http.ResponseWriter, r *http.Request) {
 			ir := h.Manager.AddItem(&i)
 			log.Println("oo: ", ir)
 			if ir.Success && ir.ID != 0 {
+				w.WriteHeader(http.StatusOK)
+				resJSON, _ := json.Marshal(ir)
+				fmt.Fprint(w, string(resJSON))
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+	}
+}
+
+func (h *ServiceHandler) UpdateItem(w http.ResponseWriter, r *http.Request) {
+	h.setContentType(w)
+	Ok := h.checkContent(r)
+	if !Ok {
+		http.Error(w, "json required", http.StatusUnsupportedMediaType)
+	} else {
+		var i manager.Item
+		pi, err := h.processBody(r, &i)
+		log.Println("bs: ", pi)
+		log.Println("err: ", err)
+		if !pi || err != nil {
+			http.Error(w, "Trouble parsing body", http.StatusBadRequest)
+		} else {
+			ir := h.Manager.UpdateItem(&i)
+			log.Println("oo: ", ir)
+			if ir.Success {
 				w.WriteHeader(http.StatusOK)
 				resJSON, _ := json.Marshal(ir)
 				fmt.Fprint(w, string(resJSON))
--- handlers/itemHandler_test.go
+++ handlers/itemHandler_test.go
@@ -65,3 +65,56 @@ func TestServiceHandler_AddItem(t *testing.T) {
 		})
 	}
 }
+
+func TestServiceHandler_UpdateItem(t *testing.T) {
+	var mm manager.MockServiceManager
+	m := mm.New()
+	var hh handlers.ServiceHandler
+	hh.Manager = m
+
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		w     http.ResponseWriter
+		r     *http.Request
+		res   manager.Response
+		code  int
+		ctype string
+		json  io.ReadCloser
+		suc   bool
+		want  int64
+		want2 bool
+	}{
+		// TODO: Add test cases.
+		{
+			code:  200,
+			ctype: "application/json",
+			json:  io.NopCloser(bytes.NewBufferString(`{"id":1, "orderId":1, "productId": 5}`)),
+			suc:   true,
+			want:  1,
+			want2: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mm.UpdateItemRes = &manager.Response{
+				Success: tt.want2,
+			}
+			// aJSON := io.NopCloser(bytes.NewBufferString(`{"orderNumber":"OD-11123", "customerID": 5}`))
+
+			r, _ := http.NewRequest("PUT", "/ffllist", tt.json)
+			r.Header.Set("Content-Type", tt.ctype)
+			w := httptest.NewRecorder()
+			// TODO: construct the receiver type.
+			//var h handlers.ServiceHandler
+			hh.UpdateItem(w, r)
+
+			var res manager.Response
+			body, _ := io.ReadAll(w.Result().Body)
+			json.Unmarshal(body, &res)
+			if w.Code != tt.code || res.Success != tt.suc {
+				t.Fail()
+			}
+		})
+	}
+}
